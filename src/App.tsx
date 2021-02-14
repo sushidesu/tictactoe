0c7fac206ee9520b9df5d6055e3e74bbe9b792f9
sushidesu <hugemerge@gmail.com>
2021-02-15 00:17:12 +0900

    font-familyを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from "react"
+import { Global, css } from "@emotion/react"
+import { Game } from "components/Game"
 
 export const App = () => {
-  return <div>tic tac toe</div>
+  return (
+    <div>
+      <Global
+        styles={css`
+          body {
+            font-family: sans-serif;
+          }
+        `}
+      />
+      <Game />
+    </div>
+  )
 }
