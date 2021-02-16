8f7b5e3eef8f4aa43903bf573d9c71287723d457
sushidesu <hugemerge@gmail.com>
2021-02-17 00:23:35 +0900

    storybookでもsrcからのインポートを使えるように修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: (config) => {
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(new TsconfigPathsPlugin())
+    } else {
+      config.resolve.plugins = [new TsconfigPathsPlugin()]
+    }
+    return config
+  },
 }
