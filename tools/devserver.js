05a68bbbb6fdbff892fa8ce8af9c00c4b1e92477
sushidesu <hugemerge@gmail.com>
2021-03-02 16:05:17 +0900

    yarn start時に勝手にブラウザを開かないように変更

--- tools/devserver.js
+++ tools/devserver.js
@@ -1,35 +1,33 @@
-const esbuild = require('esbuild');
-const cpx = require('cpx');
-const bs = require('browser-sync').create();
+const esbuild = require("esbuild")
+const cpx = require("cpx")
+const bs = require("browser-sync").create()
 
-let initialBuild;
+let initialBuild
 
 const build = async () => {
   return esbuild.build({
-    entryPoints: ['./src/index.tsx'],
+    entryPoints: ["./src/index.tsx"],
     outfile: `./build/index.js`,
-    minify: process.env.NODE_ENV === 'production',
+    minify: process.env.NODE_ENV === "production",
     incremental: true,
     bundle: true,
-    sourcemap: process.env.NODE_ENV === 'development',
+    sourcemap: process.env.NODE_ENV === "development",
     define: {
-      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
-    }
-  });
-};
+      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+    },
+  })
+}
 
-bs.watch('src/**/*.{js,jsx,ts,tsx}', (event, file) => {
-  const currentBuild = initialBuild?.rebuild() || build();
-  
-  currentBuild
-    .then(() => bs.reload('*.js'))
-    .catch((e) => console.error(e));
-});
+bs.watch("src/**/*.{js,jsx,ts,tsx}", (event, file) => {
+  const currentBuild = initialBuild?.rebuild() || build()
 
-cpx.watch('public/**/*', 'build').on('copy', (event) => {
-  bs.reload();
-});
+  currentBuild.then(() => bs.reload("*.js")).catch((e) => console.error(e))
+})
+
+cpx.watch("public/**/*", "build").on("copy", (event) => {
+  bs.reload()
+})
 
 build()
-  .then(() => bs.init({ server: './build' }))
-  .catch((e) => console.error(e));
+  .then(() => bs.init({ server: "./build", open: false }))
+  .catch((e) => console.error(e))
