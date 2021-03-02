df62500f02cd2ae810892787edaedb0d07ac5bdd
sushidesu <hugemerge@gmail.com>
2021-03-02 18:28:18 +0900

    jest実行時にe2eテストをignoreするように修正

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleDirectories: [
-    "node_modules", "src"
-  ],
-  snapshotSerializers: ["@emotion/jest/serializer"]
-};
+  snapshotSerializers: ["@emotion/jest/serializer"],
+  moduleDirectories: ["node_modules", "src"],
+  testPathIgnorePatterns: ["e2e"],
+}
