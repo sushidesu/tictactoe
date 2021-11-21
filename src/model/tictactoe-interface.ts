d71387a3dc5ca4da800b43b10452c004a6d3abb5
sushidesu <hugemerge@gmail.com>
2021-11-22 01:36:38 +0900

    use template literal type

--- src/model/tictactoe-interface.ts
+++ src/model/tictactoe-interface.ts
@@ -2,4 +2,4 @@ export type Player = "X" | "O"
 
 export type Marker = Player | "BLANK"
 
-export type Status = `WIN_X` | `WIN_O` | "DRAW" | `NEXT_X` | `NEXT_O`
+export type Status = `WIN_${Player}` | "DRAW" | `NEXT_${Player}`
